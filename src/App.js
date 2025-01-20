058e4dd2a91fbf9e2e9d5b486ad7869a1be9b2b5
dyson716 <dyson716@daum.net>
2025-01-20 17:27:27 +0900

    Add Carrot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Potato from './Potato';
 
+function Carrot() {
+  return <h3>I like Carrot</h3>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello~~~!</h1>
       <Potato />
+      <Carrot />
     </div>
   );
 }
