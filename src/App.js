bddf232067dc02de41fd9730f83d65aecad84042
dyson716 <dyson716@daum.net>
2025-01-20 17:47:41 +0900

    Use map()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from "react";
-import Potato from './Potato';
+// import Potato from './Potato';
 
-function Carrot() {
-  return <h3>I like Carrot</h3>;
+// function Carrot() {
+//   return <h3>I like Carrot</h3>;
+// }
+
+function Food (props) {
+  return (
+    <div>
+      <h1>I like {props.favorit}</h1>
+      <img src={props.picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    name: "samgyetang",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello~~~!</h1>
-      <Potato />
-      <Carrot />
+      {/* <h1>Hello~~~!</h1> */}
+      {foodILike.map(dish => (
+        <Food favorit={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
